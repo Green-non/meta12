d2ac7068a210c89eddb4e60c8ea278c27bfa2d85
Green-non <seonjingil6@gmail.com>
2026-01-14 14:44:21 +0900

    js

---
+++ front/JavaScript/script.js
@@ -0,0 +1,9 @@
+let heading = document.querySelector("#heading");
+heading.onclick = function () {
+  heading.style.color = "red";
+};
+
+// confirm("")
+document.write("<h1>어서오세요</h1>");
+let name = prompt("이름을 입력하세요", "아무개");
+console.log(username + "님, 환영합니다");
